665972914e89f253ccef02fdb3a07390d48ada34
CBiscardi <biscardi1404@gmail.com>
2022-03-22 10:34:34 -0500

    messaging Blogeek

--- public/js/general.js
+++ public/js/general.js
@@ -5,15 +5,43 @@ $(() => {
   $('.modal').modal()
 
   // TODO: Adicionar el service worker
-  
-  // Init Firebase nuevamente
+  navigator.serviceWorker.register('notificaciones-sw.js')
+    .then(registro => {
+      console.log('Service Worker Registrado')
+      firebase.messaging().useServiceWorker(registro)
+    }).catch(error =>{
+      console.error(`Error al registrar el service worker => ${error}`)      
+    })
 
+  const messaging = firebase.messaging()
   // TODO: Registrar LLave publica de messaging
-
+  messaging.usePublicVapidKey(
+    'BI5e0b_4LfTt5Tbl3ZDSXyQGvrm1Oe7y4IxE2hp7jpinJVbH2UQWDVWDJ62B7oUw4A-SN9KDGcACHGjYUeTFqSM'
+  )
   // TODO: Solicitar permisos para las notificaciones
-
+  messaging.requestPermission()
+    .then(() =>{
+      console.log('Permiso otorgado')
+      return messaging.getToken()
+    }).then(token =>{
+      const db = firebase.firestore()
+      db.collection('tokens').doc(token).set({token : token})
+        .catch(error =>{ console.log(`Error al insertar el token en la BD => ${error}`) })
+    })
+  
+    messaging.onTokenRefresh(() => {
+      messaging.getToken()
+        .then(token =>{
+          console.log('El token se ha renovado')
+          const db = firebase.firestore()
+          db.collection('tokens').doc(token).set({token : token})
+            .catch(error =>{ console.log(`Error al insertar el token en la BD => ${error}`) })
+        })
+    })
   // TODO: Recibir las notificaciones cuando el usuario esta foreground
-
+  messaging.onMessage(payload => {
+    Materialize.toast(`Ya tenemos un nuevo post. Revisalo, se llama ${payload.data.titulo}`,6000)
+  })
   // TODO: Recibir las notificaciones cuando el usuario esta background
 
   // TODO: Listening real time
--- public/notificaciones-sw.js
+++ public/notificaciones-sw.js
@@ -1,6 +1,21 @@
 importScripts('https://www.gstatic.com/firebasejs/5.5.8/firebase-app.js')
 importScripts('https://www.gstatic.com/firebasejs/5.5.8/firebase-messaging.js')
 
+firebase.initializeApp({
+    projectId: "blogeekplatzi-e48f1",
+    messagingSenderId: "331273176127"
+})
 
+const messaging = firebase.messaging()
+
+messaging.setBackgroundMessageHandler(payload =>{
+    const tituloNotificacion = 'Ya tenemos un nuevo Post'
+    const opcionesNotificacion = {
+        body: payload.data.titulo,
+        icon: 'icons/icon_new_post.png',
+        click_action: "https://blogeekplatzi-e48f1.web.app"
+    }
+    return self.ServiceWorkerRegistration.showNotificacion(tituloNotificacion, opcionesNotificacion)
+})
 
 
